fb763ea97a55c45848c42b24f363ab665bcfce57
marco <marcogomesr@gmail.com>
2023-08-17 16:44:37 +0200

    config prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
 import './App.css'
 
 function App() {
-
-  return (
-      <h1 className="text-3xl font-bold underline bg-red-400">Vite + React</h1>
-  )
+  return <h1 className="grid text-3xl font-bold underline">Vite + React</h1>
 }
 
 export default App
